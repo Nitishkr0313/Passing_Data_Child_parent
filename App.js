4e3d350931a79934419052490f48f3eb741e122f
Nitish_kr_Singh <88366599+Nitishkr0313@users.noreply.github.com>
2022-09-01 19:14:19 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import Parent from "./Parent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Set the data child to parent component</h2>
+      <hr />
+      <Parent />
+      <hr />
+    </div>
+  );
+}
